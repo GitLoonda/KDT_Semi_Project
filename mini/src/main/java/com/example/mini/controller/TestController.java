8c0d8dcaf5134a8e0ba27564ccb76bfffe574cdb
tj <tj@DESKTOP-7H5A484>
2023-05-03 12:52:32 +0900

    test

---
+++ mini/src/main/java/com/example/mini/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.mini.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/test123.do") 
+    public String insert(Model model) throws Exception{
+
+        return "/test123";
+    }
+
+}
+
+
