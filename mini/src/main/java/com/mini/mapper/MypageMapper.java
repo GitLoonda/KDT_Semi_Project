9e24ba4b70bc024f25773225f8b086492db8837c
roalwh <83705507+roalwh@users.noreply.github.com>
2023-05-04 18:56:36 +0900

    Delete mini directory

--- mini/src/main/java/com/mini/mapper/MypageMapper.java
+++
@@ -1,8 +0,0 @@
-package com.mini.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MypageMapper {
-
-}
--- mini/src/main/java/com/mini/service/AdminService.java
+++
@@ -1,5 +0,0 @@
-package com.mini.service;
-
-public interface AdminService {
-
-}
