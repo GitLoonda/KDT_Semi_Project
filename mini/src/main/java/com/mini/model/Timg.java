6ecba70ab8f3788bc8ca45d353d60415de798450
roalwh <nmnmnmlk6248@gmail.com>
2023-05-19 19:41:56 +0900

    20230519 timg

---
+++ mini/src/main/java/com/mini/model/Timg.java
@@ -0,0 +1,11 @@
+package com.mini.model;
+
+import lombok.Data;
+
+public class Timg {
+    private int tpathno;
+    private int tbno;
+    private String path;
+    private String rep;
+//    ddddddd
+}
