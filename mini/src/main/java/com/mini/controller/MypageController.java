c3b99d8c987c7613ee5a546d7985dd42996c87cc
ljsa0322 <48462617+ljsa0322@users.noreply.github.com>
2023-05-23 15:58:33 +0900

    Add files via upload

--- mini/src/main/java/com/mini/controller/MypageController.java
+++ mini/src/main/java/com/mini/controller/MypageController.java
@@ -1,5 +1,7 @@
 package com.mini.controller;
 
+import java.io.File;
+import java.util.Calendar;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.google.gson.Gson;
 import com.mini.model.Mypg;
 import com.mini.service.MypageService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -95,6 +100,69 @@ public class MypageController {
 		resultMap.put("result", "success");
 		return new Gson().toJson(resultMap);
 	}
+    
+    @RequestMapping("/mypage/upload.dox")
+    public String result(@RequestParam("file1") MultipartFile multi, @RequestParam("id") String id, HttpServletRequest request,HttpServletResponse response, Model model)
+    {
+        String url = null;
+        String path="c:\\img";
+        try {
+ 
+            //String uploadpath = request.getServletContext().getRealPath(path);
+            String uploadpath = path;
+            String originFilename = multi.getOriginalFilename();
+            String extName = originFilename.substring(originFilename.lastIndexOf("."),originFilename.length());
+            long size = multi.getSize();
+            String saveFileName = genSaveFileName(extName);
+            
+            System.out.println("uploadpath : " + uploadpath);
+            
+            System.out.println("originFilename : " + originFilename);
+            System.out.println("extensionName : " + extName);
+            System.out.println("size : " + size);
+            System.out.println("saveFileName : " + saveFileName);
+            String path2 = System.getProperty("user.dir");
+            System.out.println("Working Directory = " + path2 + "src\\webapp\\img\\mypage");
+            if(!multi.isEmpty())
+            {
+                File file = new File(path2 + "\\src\\main\\webapp\\img\\mypage", saveFileName);
+                multi.transferTo(file);
+                
+                HashMap<String, Object> map = new HashMap<String, Object>();
+                map.put("img", "\\img\\mypage\\" + saveFileName);
+                map.put("id", id);
+                
+                // insert 쿼리 실행
+                mypageService.addMyPageImg(map); 
+                
+                model.addAttribute("filename", multi.getOriginalFilename());
+                model.addAttribute("uploadPath", file.getAbsolutePath());
+                
+                return "filelist";
+            }
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+        return "redirect:main.do";
+    }
+    
+    
+ // 현재 시간을 기준으로 파일 이름 생성
+    private String genSaveFileName(String extName) {
+        String fileName = "";
+        
+        Calendar calendar = Calendar.getInstance();
+        fileName += calendar.get(Calendar.YEAR);
+        fileName += calendar.get(Calendar.MONTH);
+        fileName += calendar.get(Calendar.DATE);
+        fileName += calendar.get(Calendar.HOUR);
+        fileName += calendar.get(Calendar.MINUTE);
+        fileName += calendar.get(Calendar.SECOND);
+        fileName += calendar.get(Calendar.MILLISECOND);
+        fileName += extName;
+        
+        return fileName;
+    }
     
 
     
